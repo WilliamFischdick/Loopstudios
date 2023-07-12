69e6d066a813cd180b9dc29fc1d48641cdbad310
William Fischdick <jwfbit@hotmail.com>
2023-07-11 22:43:39 -0300

    :construction: feat: Adicionando a funcionalidade do menu hamburger com JavaScript.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-const button = document.querySelector(".container__tag");
+const hamburguer = document.querySelector(".hamburger")
+const navMenu = document.querySelector(".nav__menu");
+
+hamburguer.addEventListener("click", () => {
+    hamburguer.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    
+})
+
+document.querySelectorAll(".nav__link").forEach(n => n.addEventListener("click",() => {
+    hamburguer.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
