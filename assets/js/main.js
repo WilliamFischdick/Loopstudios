7306c363546c11c5ef906cfcc77052c1d1bae533
William Fischdick <jwfbit@hotmail.com>
2023-07-12 21:54:25 -0300

    :lipstick: Posicionando melhor o menu hamgurger ativo.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,16 @@
 const hamburguer = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".nav__menu");
+const navBar = document.querySelector(".navbar");
 
 hamburguer.addEventListener("click", () => {
     hamburguer.classList.toggle("active");
     navMenu.classList.toggle("active");
+    navBar.classList.toggle("active");
     
 })
 
 document.querySelectorAll(".nav__link").forEach(n => n.addEventListener("click",() => {
     hamburguer.classList.remove("active");
     navMenu.classList.remove("active");
+    navBar.classList.remove("active");
 }))
