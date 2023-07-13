c17cb6bd306d8243c6fc5c2863b69078de72ac42
William Fischdick <jwfbit@hotmail.com>
2023-07-12 22:06:40 -0300

    :lipstick: corrigindo o fundo do header ao ativar o menu e excluindo comentários no código.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,16 @@
 const hamburguer = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".nav__menu");
-const navBar = document.querySelector(".navbar");
+const header = document.querySelector("header");
 
 hamburguer.addEventListener("click", () => {
     hamburguer.classList.toggle("active");
     navMenu.classList.toggle("active");
-    navBar.classList.toggle("active");
+    header.classList.toggle("active");
     
 })
 
 document.querySelectorAll(".nav__link").forEach(n => n.addEventListener("click",() => {
     hamburguer.classList.remove("active");
     navMenu.classList.remove("active");
-    navBar.classList.remove("active");
+    header.classList.remove("active");
 }))
